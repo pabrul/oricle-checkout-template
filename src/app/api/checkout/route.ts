1af0a8b35afd31940aeb656c52d85d2f875abfd2
pabrul <pablohrbatista@gmail.com>
2025-01-20 20:58:05 -0300

    fix: using log in the error

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,13 +2,15 @@ import { NextResponse } from 'next/server';
 import { getCheckoutData } from '@/lib/checkout';
 
 export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const template = searchParams.get('template') ? Number(searchParams.get('template')) : 1;
-
-  try {
-    const checkoutData = await getCheckoutData(template);
-    return NextResponse.json(checkoutData);
-  } catch (error) {
-    return NextResponse.json({ error: 'Failed to load data' }, { status: 500 });
+    const { searchParams } = new URL(req.url);
+    const template = searchParams.get('template') ? Number(searchParams.get('template')) : 1;
+  
+    try {
+      const checkoutData = await getCheckoutData(template);
+      return NextResponse.json(checkoutData);
+    } catch (error) {
+      console.error('Error fetching checkout data:', error);
+      return NextResponse.json({ error: 'Failed to load data' }, { status: 500 });
+    }
   }
-}
+  
